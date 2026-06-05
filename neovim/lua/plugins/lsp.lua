364f20c8032f8e2d55b8be6c24c754b75099cb30
Nick <ndemco@gmail.com>
2026-06-04 20:49:35 -0400

    idk

--- neovim/lua/plugins/lsp.lua
+++ neovim/lua/plugins/lsp.lua
@@ -25,6 +25,13 @@ return {
         },
       })
 
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+          vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
+        end,
+      })
+
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(event)
           local map = function(keys, func, desc)
